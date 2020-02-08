6bbed0f0a47cffc39eb3c51a90aebed3b60430f3
Tom Durrant <tomchdurrant@gmail.com>
2020-02-08 18:38:22 +0000

    Completed sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) {
+        return 'ERROR';
+    }
+    let largerNumber = Math.max(a, b);
+    let smallerNumber = Math.min(a, b);
+    let runningTotal = 0;
+    for (let i = smallerNumber; i <= largerNumber; i++) {
+        runningTotal += i;
+    }
+    return runningTotal;
 }
 
 module.exports = sumAll
