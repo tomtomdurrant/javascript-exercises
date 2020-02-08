2a062f953de7afc13015722344a6ef0c7e41158c
Tom Durrant <tomchdurrant@gmail.com>
2020-02-08 15:29:33 +0000

    Completed Remove from Array

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (inputArray, ...args) {
+    console.log(inputArray);
+    console.log(args);
 
+
+    return inputArray.filter(v => !args.includes(v))
 }
+
+console.log(removeFromArray([1, 2, 3, 4], 3, 2));
+
+
 
 module.exports = removeFromArray
