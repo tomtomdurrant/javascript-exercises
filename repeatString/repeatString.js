93d7ffa997bcfd28f467ee76182822335d9c4372
Tom Durrant <tomchdurrant@gmail.com>
2020-02-08 14:45:18 +0000

    Completed Hello world and Repeat String

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (input, number) {
+    if (number < 0) {
+        return 'ERROR';
+    }
+    let output = '';
+    for (let i = 0; i < number; i++) {
+        output += input;
+    }
+    return output;
 }
 
 module.exports = repeatString
