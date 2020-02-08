5c17e97c4054c726166312987bfc6d1f874bef36
Tom Durrant <tomchdurrant@gmail.com>
2020-02-08 18:57:05 +0000

    Completed tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function (temperature) {
+  const celsius = (temperature - 32) * 5 / 9;
+  return +celsius.toFixed(1);
 }
 
-const ctof = function() {
-
+const ctof = function (temperature) {
+  const fahrenheit = temperature * 9 / 5 + 32;
+  return +fahrenheit.toFixed(1);
 }
 
 module.exports = {
